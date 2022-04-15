ac974776515929d327e48a3855c25dc8107abf75
Aniket1675 <Aniket@05>
2022-04-15 11:06:01 +0530

    ADDED: Writing to Json file and Reading From Json File

--- src/main/java/com/bridgelabz/AddressBookIF.java
+++ src/main/java/com/bridgelabz/AddressBookIF.java
@@ -7,18 +7,34 @@ import java.io.IOException;
 
 public interface AddressBookIF {
     public void addContacts();
-    public void edit();
-    public void delete();
-    public void display();
-    public void searchByCity(String city,String firstName);
-    public void searchByState(String state, String firstName);
-    public void personsInCity(String city);
-    public void personsInState(String State);
-    public  void sortByFirstName();
-    public  void sortByZip();
-    public  void sortByCity();
-    public  void sortByState();
-    public void writeDataToCSV() throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException;
-    public <CsvValidationException extends Throwable> void readDataFromCSV() throws IOException, CsvValidationException;
 
+    public void edit();
+
+    public void delete();
+
+    public void display();
+
+    public void searchByCity(String city, String firstName);
+
+    public void searchByState(String state, String firstName);
+
+    public void personsInCity(String city);
+
+    public void personsInState(String State);
+
+    public void sortByFirstName();
+
+    public void sortByZip();
+
+    public void sortByCity();
+
+    public void sortByState();
+
+    public void writeDataToJson() throws IOException;
+
+    public void readDataFromJson() throws IOException;
+
+    public void writeDataToCSV() throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException;
+
+    public <CsvValidationException extends Throwable> void readDataFromCSV() throws IOException, CsvValidationException;
 }
